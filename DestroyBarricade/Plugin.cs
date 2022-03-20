b9c486a0edf2f8e1f5911e8dd320c6758dca4a49
IdiosGame <36539661+HuySan@users.noreply.github.com>
2022-03-20 14:43:49 +0300

    Add

---
+++ DestroyBarricade/Plugin.cs
@@ -0,0 +1,53 @@
+﻿using Rocket.Core.Plugins;
+using SDG.Unturned;
+using System.Linq;
+
+namespace BarricadeDamage
+{
+    public class Plugin : RocketPlugin<Config>
+    {
+    
+        protected override void Load()
+        {
+             if (Configuration.Instance.barricadeSettings.enabled)
+             {
+                 var time = Configuration.Instance.barricadeSettings.minutesForDamage * 60;
+                 InvokeRepeating("Damage", time, time);
+             }
+        }
+
+        public void Damage()
+        {
+            ClearBarricade();              
+        }
+
+
+        private void ClearBarricade()
+        {
+            int i = 0;
+            foreach (var region in BarricadeManager.regions)
+                {               
+                    foreach(var drop in region.drops.ToList())//ToList() - foreva
+                    {
+                        
+                        Rocket.Core.Logging.Logger.Log("============" + drop.asset.name + "=====================================");
+                        if(BarricadeManager.FindBarricadeByRootTransform(drop.model) != null)
+                        {                           
+                          if (Configuration.Instance.barricadeSettings.IsInWhiteList(drop.asset.id)) continue;
+                        //Interactable2HP component = barricadeRegion.drops[(int)22].model.GetComponent<Interactable2HP>();                      
+                          BarricadeManager.damage(drop.model, (drop.asset.health / 100) * Configuration.Instance.barricadeSettings.percentDamage, 1, false);                      
+                        }
+                        i++;
+                    }
+                } 
+                Rocket.Core.Logging.Logger.Log("Damage to the barricades was committed, damaged " + i + " barricades");
+        }
+
+
+        protected override void Unload()
+        {
+
+            CancelInvoke("Damage");
+        }
+    }
+}
---
+++ DestroyBarricade/Types/BarricadeSettings.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace DestroyBarricade.Types
+{
+    public class BarricadeSettings
+    {
+        [XmlAttribute]
+        public bool enabled;
+        [XmlAttribute]
+        public int minutesForDamage;
+        [XmlAttribute]
+        public int percentDamage;
+        [XmlArray]
+        public List<int> WhiteList;
+
+        public BarricadeSettings(bool enabled, int minutesForDamage, int percentDamage, List<int> WhiteList)
+        {
+            this.enabled = enabled;
+            this.minutesForDamage = minutesForDamage;
+            this.WhiteList = WhiteList;
+            this.percentDamage = percentDamage;
+        }
+
+        public BarricadeSettings() { }
+
+        public bool IsInWhiteList(uint idBarricade)
+        {
+            foreach(int id in WhiteList)
+            {
+                if(idBarricade == id) return true; 
+            }
+            return false;
+        }
+    }
+}
